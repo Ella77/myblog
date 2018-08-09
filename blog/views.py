427241746abc7dd17c32fb12bacea83d90440a12
Ella <ella@mykoon.com>
2018-08-09 16:26:09 +0900

    modified templates from database

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Post
+from  django.utils  import timezone
 
 # Create your views here.
 def post_list(request):
-    return render(request,'blog/post_list.html')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    # posts = Post.objects.all()
+    return render(request,'blog/post_list.html',{'posts': posts})
+
+
+# 'blogs' 매개변수에 필터링된 게시물들 추가
+
