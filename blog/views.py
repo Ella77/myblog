69fe2c5ba05c1af619ae7259f383606b45238e20
Ella <ella@mykoon.com>
2018-08-09 17:57:04 +0900

    Added views to create/edit

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from  django.utils  import timezone
+from .forms import PostForm
+
 
 # Create your views here.
 def post_list(request):
@@ -8,6 +10,36 @@ def post_list(request):
     # posts = Post.objects.all()
     return render(request,'blog/post_list.html',{'posts': posts})
 
-
+def post_detail(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 # 'blogs' 매개변수에 필터링된 게시물들 추가
+
+def post_new(request):
+    if request.method =="POST" :
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+            post = form.save(commit = False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else :
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST" :
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit = False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form = PostForm(instance = post)
+    return render(request, 'blog/post_edit.html',{'form':form})
 
