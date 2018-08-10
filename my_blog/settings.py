470608e7e10ec61c3c7fedfd9e211291053ac67e
Ella <ella@mykoon.com>
2018-08-10 17:56:36 +0900

    blog comment, login

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +121,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+LOGIN_REDIRECT_URL ='/'
--- my_blog/urls.py
+++ my_blog/urls.py
@@ -1,8 +1,14 @@
 
 from django.contrib import admin
 from django.conf.urls import url,include
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    # url(r'^accounts/login/$', auth_views.LoginView.as_view(template_name ='registration/login.html')),
     url(r'', include('blog.urls')),
+    # url(r'^accounts/logout/$', auth_views.LogoutView.as_view(template_name ='registration/login.html')),
+    url('accounts/login/$', auth_views.LoginView.as_view(), name ='login'),
+url('accounts/logout/$', auth_views.LogoutView.as_view(), name='logout'),
 ]
